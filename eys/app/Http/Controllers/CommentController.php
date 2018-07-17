59045e8357bf7b07320fa78feb7aaaa4f493dd1c
Jordi Prats <jordi.prats@gmail.com>
2018-07-17 16:39:34 +0200

    store comment

--- eys/app/Http/Controllers/CommentController.php
+++ eys/app/Http/Controllers/CommentController.php
@@ -10,4 +10,28 @@ class CommentController extends Controller
   {
     $this->middleware('auth');
   }
+
+  public function store(Request $request)
+  {
+    // validate
+    $this->validate($request, array(
+      'ticket_id' => 'required|integer',
+      'user_id' => 'required|integer',
+      'description' => 'string',
+    ));
+
+    if(Auth::user()->id != $request->user_id)
+    {
+      // trying to add a comment using a different user id - fuck off
+      return redirect()->route('home');
+    }
+    else
+    {
+      $comment = Comment::create([
+                                    'ticket_id'   => $request->ticket_id,
+                                    'user_id'     => $request->user_id,
+                                    'description' => $request->description,
+                                  ]);
+    }
+  }
 }
