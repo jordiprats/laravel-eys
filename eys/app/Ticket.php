dd65c5a9f8bab9cc294de10a8f51a3958aa129ab
Jordi Prats <jordi.prats@gmail.com>
2018-07-19 17:29:58 +0200

    generar ticket number "bonic"

--- eys/app/Ticket.php
+++ eys/app/Ticket.php
@@ -10,6 +10,12 @@ class Ticket extends Model
 {
   protected $guarded = [];
 
+  public function getTicketID()
+  {
+    $hashids = new \Jenssegers\Optimus\Optimus(1580030173, 59260789, 1163945558);
+    return $hashids->encode($this->id);
+  }
+
   public function comments()
   {
     return $this->hasMany(Comment::class)->where('user_id', Auth::user()->id)->orderBy('created_at', 'desc');
